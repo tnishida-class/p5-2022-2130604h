9e7bb08cb3b633bee9e095d24533af300ceead36
2130604h <114968660+2130604h@users.noreply.github.com>
2022-10-04 12:08:17 +0900

    Change section1-1 to Sakura-zaka

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,13 +3,13 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(255);
   strokeWeight(4);
   fill(178, 100, 162);
-  triangle(1,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(5,108,106,108,106,0);
+  triangle(12,120,116,120,116,10);
   fill(255);
+  triangle(0,108,106,108,106,0);
+  fill(116, 37, 129);
   textSize(20);
   textFont("serif");
   text("46", 68, 100);
