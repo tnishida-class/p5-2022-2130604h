1a9c407ea3ae109c39752bc1c41792a032fb9c89
2130604h <114968660+2130604h@users.noreply.github.com>
2022-10-11 15:04:34 +0900

    Change section2-1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(100,100);
+  noFill();
+  for(let i = 1; i < 11; i=i+1){
     // BLANK[1]
-  }
+    if(i < 5){
+      stroke(0,0,255);
+      ellipse(50,50, i*10);
+    }
+    else{
+      stroke(255,0,0);
+      ellipse(50,50,i*10);
+    }
+}
 }
